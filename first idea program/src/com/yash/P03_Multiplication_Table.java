8c9d50a7374927e6edf939b8c0183ca9e377af34
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-01 00:14:30 +0530

    github

---
+++ first idea program/src/com/yash/P03_Multiplication_Table.java
@@ -0,0 +1,18 @@
+package com.yash;
+
+import java.util.Scanner;
+
+public class P03_Multiplication_Table {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter the number :");
+        int num = sc.nextInt();
+
+        System.out.println("multiplication table of a number :" + num);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+        sc.close();
+    }
+}
---
+++ first idea program/src/com/yash/P04_Sum_Until_X.java
@@ -0,0 +1,21 @@
+package com.yash;
+
+import java.util.Scanner;
+
+public class P04_Sum_Until_X {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        System.out.println("enter the number(type x to stop)");
+
+        while (sc.hasNextInt()) {
+            System.out.println("enter the number(type x to stop)");
+            int num = sc.nextInt();
+            sum = sum + num;
+        }
+        System.out.println("sum of all the numbers " + sum);
+        sc.close();
+    }
+}
