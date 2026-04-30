c66e30b0742069281aeb2060aa9b5a585ec9dfe5
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-04-30 23:42:45 +0530

    github

---
+++ first idea program/src/com/yash/P02_Sum_Of_Two_Numbers.java
@@ -0,0 +1,16 @@
+package com.yash;
+
+import java.util.Scanner;
+
+public class P02_Sum_Of_Two_Numbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the num1 :");
+        int num1 = sc.nextInt();
+        System.out.print("enter the num2 :");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("sum of "+num1+" and "+num2+" is "+ sum);
+    }
+}
