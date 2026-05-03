ffe2652318a7604ce4e2550e9b2e95a1253a09f8
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-03 21:40:16 +0530

    practice

---
+++ first idea program/src/com/yash/f02_practice/P06_Fibonacci_Series.java
@@ -0,0 +1,21 @@
+package com.yash.f02_practice;
+
+public class P06_Fibonacci_Series {
+
+
+    public static void Logic() {
+        int a = 0;
+        int b = 1;
+        for (int i = 0; i <= 10; i++) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        P06_Fibonacci_Series.Logic();
+    }
+}
