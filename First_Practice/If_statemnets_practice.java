b547e9cafed746c38dd867539f76d0cf1d6269f9
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-06 23:21:16 +0530

    Update If_statemnets_practice.java

--- First_Practice/If_statemnets_practice.java
+++ First_Practice/If_statemnets_practice.java
@@ -9,18 +9,31 @@ class Calculatar {
     
     
     // print numbers from 1 to 5
-    
+    // for loop
     for(int num = 1; num <=n; num++){
         
         SyStem.out.print(num);
         
     }
+    
+    // while loop 
     int num = 1
     
     while(num <= n){
         SyStem.out.print(num);
         num ++; 
         
+    }
+    
+    
+    // do while loop(atleast runs the program once)
+    
+    do{
+             SyStem.out.println(" " hello world);
+        
+    }while (num <=n){
+               num ++;
+
     }
         }
 }
