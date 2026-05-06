504139b293fd6a7e88147102e7ebd4a24a15b718
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-06 23:05:32 +0530

    Create If_statemnets_practice.java

---
+++ First_Practice/If_statemnets_practice.java
@@ -0,0 +1,19 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Calculatar {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(Syatem.in);
+    int n = sc.nextInt();
+    SyStem.out.print("enter the number ");
+    
+    
+    // print numbers from 1 to 5
+    
+    for(int num = 1; num <=n; num++){
+        
+        SyStem.out.print(n);
+        
+    }
+        }
+}
