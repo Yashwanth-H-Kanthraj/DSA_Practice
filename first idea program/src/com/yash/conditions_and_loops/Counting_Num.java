b47574ae25ff34ea7fa16ed032ec5c2e8cc35339
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-07 23:48:04 +0530

    practice

---
+++ first idea program/src/com/yash/conditions_and_loops/Counting_Num.java
@@ -0,0 +1,22 @@
+package com.yash.conditions_and_loops;
+
+public class Counting_Num {
+
+    static void main(String[] args) {
+
+
+        int number = 303251555;
+
+        int count = 0;
+        while (number > 0) {
+
+            int rem = number % 10;
+            if (rem == 5) {
+                count++;
+            }
+            number = number / 10;
+        }
+        System.out.println(count);
+
+    }
+}
