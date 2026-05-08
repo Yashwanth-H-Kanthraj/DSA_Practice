b02538900205a16644a650b225be20a6ecede29c
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-08 22:26:57 +0530

    practice

---
+++ first idea program/src/com/yash/conditions_and_loops/calculator.java
@@ -0,0 +1,43 @@
+package com.yash.conditions_and_loops;
+
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+        while (true) {
+            System.out.print("enter the operator");
+            char op = sc.next().trim().charAt(0);
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.print("enter the two numbers");
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+
+                if (op == '+') {
+                    ans = n1 + n2;
+                }
+                if (op == '-') {
+                    ans = n1 - n2;
+                }
+                if (op == '*') {
+                    ans = n1 * n2;
+                }
+                if (op == '/') {
+                    ans = n1 / n2;
+                }
+                if (op == '%') {
+                    ans = n1 % n2;
+                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+            } else {
+                System.out.println("invalid operator");
+            }
+
+
+        }
+    }
+}
