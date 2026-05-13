fb3f6ed76c96339a303e5316c7ba2c88d6840b05
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-13 23:04:25 +0530

    practice

---
+++ First_Practice/LinkedList1.java
@@ -0,0 +1,22 @@
+package com;
+import java.util.LinkedList;
+
+public class LinkedList1 {
+
+	public static void main(String[] args) {
+		LinkedList l = new LinkedList();
+				
+		l.add(10);
+		l.add(20);
+		l.add(10);
+		l.add(30);
+		
+		System.out.println(l);
+		System.out.println(l.indexOf(10));
+		System.out.println(l.lastIndexOf(30));
+		
+		
+		
+	}
+
+}
