ba4e3cac9e9391b44d5ceb015da0dcd95331b038
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-15 22:33:08 +0530

    practice

---
+++ first idea program/src/com/yash/f03_conditions_and_loops/P15_Maximum_Integer_Number.java
@@ -0,0 +1,25 @@
+package com.yash.f03_conditions_and_loops;
+
+import java.util.Scanner;
+
+public class P15_Maximum_Integer_Number {
+    public static void main(String[] args) {
+
+        int largest = Integer.MIN_VALUE;
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("enter the number :");
+            int num = sc.nextInt();
+
+            if (num == 0) {
+                break;
+            } else if (num > largest) {
+                largest = num;
+            }
+        }
+        System.out.print("Maximum number : " + largest);
+    }
+}
+
+
