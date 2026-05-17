1834ded56bf946c6dbe54f1df4f5c2ded2e4e679
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-17 22:10:01 +0530

    practice

---
+++ first idea program/src/com/yash/f03_conditions_and_loops/intermediateProblems/P2_Electricity_Bill.java
@@ -0,0 +1,37 @@
+package com.yash.f03_conditions_and_loops.intermediateProblems;
+
+import java.util.Scanner;
+
+public class P2_Electricity_Bill {
+    public static void main(String[] args) {
+        System.out.println(" program started ");
+        double units = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter how many units have been consumed : ");
+        units = sc.nextDouble();
+
+
+        if (units < 0 || units > 1000) {
+            System.out.println("invalid units entered. cannot be calculated. ");
+        }
+
+        if (units <= 100) {
+            double useage = units * 1.5;
+            System.out.printf("bill generated for the %.3f units consumed is %.2f Rs. ", units, useage);
+        } else if (units <= 200) {
+
+            double useage = ((units - 100) * 2.50) + (100 * 1.50);
+            System.out.printf("bill generated for the %.3f units consumed is %.2f Rs. ", units, useage);
+        } else if (units <= 300) {
+
+            double useage = ((units - 200) * 4.0) + (100 * 1.50) + (100 * 2.50);
+            System.out.printf("bill generated for the %.3f units consumed is %.2f Rs. ", units, useage);
+        } else {
+
+            double useage = ((units - 300) * 5.0) + (100 * 1.50) + (100 * 2.5) + (100 * 4);
+            System.out.printf("bill generated for the %.3f units consumed is %.2f Rs. ", units, useage);
+        }
+        sc.close();
+    }
+}
