2af70b9a9be0288997891397c4ea034723bd90a4
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-18 20:34:12 +0530

    practice

---
+++ Strivers DSA/If_statemnets_practice.java
@@ -0,0 +1,39 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Calculatar {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(Syatem.in);
+    int n = sc.nextInt();
+    SyStem.out.print("enter the number ");
+    
+    
+    // print numbers from 1 to 5
+    // for loop
+    for(int num = 1; num <=n; num++){
+        
+        SyStem.out.print(num);
+        
+    }
+    
+    // while loop 
+    int num = 1
+    
+    while(num <= n){
+        SyStem.out.print(num);
+        num ++; 
+        
+    }
+    
+    
+    // do while loop(atleast runs the program once)
+    
+    do{
+             SyStem.out.println(" " hello world);
+        
+    }while (num <=n){
+               num ++;
+
+    }
+        }
+}
