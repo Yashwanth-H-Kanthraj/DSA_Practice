682ab44fb97c6001e036f9e1202b78ca010ab2dd
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-19 23:12:13 +0530

    practice

--- Strivers DSA/src/com/patterns/Q10.java
+++ Strivers DSA/src/com/patterns/Q10.java
@@ -1,4 +1,21 @@
 package com.patterns;
 
 public class Q10 {
+    static void patternup(int n) {
+
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars = i;
+            if (i > n) stars = 2 * n - i;
+
+            for (int j = 1; j <= stars; j++) {
+
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        patternup(5);
+    }
 }
