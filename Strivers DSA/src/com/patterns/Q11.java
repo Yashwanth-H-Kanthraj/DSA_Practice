dde93cd749bddf3b08fe34a34fd386853591c410
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-19 23:47:06 +0530

    practice

--- Strivers DSA/src/com/patterns/Q11.java
+++ Strivers DSA/src/com/patterns/Q11.java
@@ -1,4 +1,34 @@
 package com.patterns;
 
 public class Q11 {
+
+    /*
+    row         pattern         column
+    1           1               1
+    2           01              2
+    3           101             3
+     */
+
+    int start;
+
+    public void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+
+            if (i % 2 == 0) start = 1;
+            else start = 0;
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start);
+                start = 1 - start;
+
+            }
+            System.out.println();
+        }
+    }
+
+
+    static void main(String[] args) {
+        Q11 obj = new Q11();
+        obj.pattern(5);
+    }
 }
