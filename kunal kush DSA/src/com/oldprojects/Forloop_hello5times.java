c69d6153cf0e573c828aadf7d2e0bfff34366c62
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-20 20:11:16 +0530

    practice

---
+++ kunal kush DSA/src/com/oldprojects/Forloop_hello5times.java
@@ -0,0 +1,9 @@
+package com;
+//WAJP to print hello 5 times.
+public class Forloop_hello5times {
+	public static void main(String[] args)
+	{
+		for (int i = 1; i <= 5; i++) 
+		{
+			System.out.println("hello");
+		}}}
