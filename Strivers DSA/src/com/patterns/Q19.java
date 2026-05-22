b2e6ec5e182cae31820cb24c4dac906c373b7cb9
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-22 20:58:07 +0530

    practice

--- Strivers DSA/src/com/patterns/Q19.java
+++ Strivers DSA/src/com/patterns/Q19.java
@@ -3,25 +3,37 @@ package com.patterns;
 public class Q19 {
     public static void main(String[] args) {
         Q19 obj = new Q19();
+        obj.pattern(5);
         obj.pattern2(5);
 
     }
 
     public void pattern2(int n) {
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n-i; j++) {
+            for (int j = 0; j < n - i; j++) {
                 System.out.print("*");
             }
-            for (int j = 0; j < 2*i; j++) {
+            for (int j = 0; j < 2 * i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; j <n-i; j++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
-
 
     public void pattern(int n) {
         for (int i = 0; i < n; i++) {
