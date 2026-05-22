f7423c317705e31303593320d4cb09c21eb446c8
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-22 23:29:39 +0530

    practice

--- Strivers DSA/src/com/patterns/Q20.java
+++ Strivers DSA/src/com/patterns/Q20.java
@@ -1,32 +1,24 @@
 package com.patterns;
 
 public class Q20 {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         Q20 obj = new Q20();
         obj.pattern(5);
     }
 
     public void pattern(int n) {
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n; j > i; j--) {
+        for (int i = 1; i < 2 * n; i++) {
+            int stars = i;
+            if (i > n) stars = 2 * (n) - i;
+            for (int j = 0; j < stars; j++) {
                 System.out.print("*");
             }
-            for (int j = 1; j <= 2 * i; j++) {
+            int space = 2 * (n - i);
+            if (i > n) space = 2 * (i - n);
+            for (int j = 0; j < space; j++) {
                 System.out.print(" ");
             }
-            for (int j = n; j > i; j--) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = n; j > i; j--) {
-                System.out.print("*");
-            }
-            for (int j = 1; j <= 2 * i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = n; j > i; j--) {
+            for (int j = 0; j < stars; j++) {
                 System.out.print("*");
             }
             System.out.println();
