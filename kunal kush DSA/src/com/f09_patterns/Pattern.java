1211a3d47a2c58a336d4a5ba6a8939aa13514e29
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-26 00:57:08 +0530

    practice

--- kunal kush DSA/src/com/f09_patterns/Pattern.java
+++ kunal kush DSA/src/com/f09_patterns/Pattern.java
@@ -3,7 +3,7 @@ package com.f09_patterns;
 public class Pattern {
     static void main(String[] args) {
         Pattern obj = new Pattern();
-        obj.pattern6(5);
+        obj.pattern8(5);
     }
 
     public void pattern1(int n) {
@@ -60,7 +60,7 @@ public class Pattern {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            for (int j = n - i+1; j <= n ; j++) {
+            for (int j = n - i + 1; j <= n; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -69,13 +69,29 @@ public class Pattern {
 
     public void pattern7(int n) {
         for (int i = 0; i < n; i++) {
-            for
-
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-
     }
 
-    public void pattern8() {
+    public void pattern8(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
 
     }
 
