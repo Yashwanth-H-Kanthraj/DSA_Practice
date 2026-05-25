0fe6db4023d023de7763d1116899a04cb87aa48b
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-25 21:27:06 +0530

    practice

--- Strivers DSA/src/com/patterns/Q21.java
+++ Strivers DSA/src/com/patterns/Q21.java
@@ -1,4 +1,33 @@
 package com.patterns;
 
 public class Q21 {
+    static void main(String[] args) {
+        Q21 obj = new Q21();
+        obj.pattern(10);
+    }
+
+    /*
+    row     pattern     column
+    0       ****        4stars for 1st and last
+    1       *  *        2stars 2 stars from 2nd row
+    2       *  *        3stars
+    3       ****        4stars
+
+
+     */
+    public void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == n - 1 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+
+
 }
