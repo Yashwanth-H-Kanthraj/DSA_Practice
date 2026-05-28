af28eac035d529938082e688414f6241a0066570
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-05-28 21:40:20 +0530

    practice

--- kunal kush DSA/src/com/f09_patterns/Pattern.java
+++ kunal kush DSA/src/com/f09_patterns/Pattern.java
@@ -359,7 +359,7 @@ public class Pattern {
     public void pattern26(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j <= n - i; j++) {
-                System.out.print(i);
+                System.out.print(i+" ");
             }
             System.out.println();
         }
