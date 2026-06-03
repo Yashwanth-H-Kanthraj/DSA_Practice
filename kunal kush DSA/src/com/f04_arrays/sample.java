6215c02409132a212cacef6745199761884a0e37
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-06-03 23:07:59 +0530

    practice

--- kunal kush DSA/src/com/f04_arrays/sample.java
+++ kunal kush DSA/src/com/f04_arrays/sample.java
@@ -7,9 +7,6 @@ public class sample {
     public static void main(String[] args) {
         sample s = new sample();
         s.search();
-
-
-
     }
 
     public void search() {
@@ -20,5 +17,18 @@ public class sample {
             }
         }
         System.out.println(max);
+    }
+
+
+    public void search1() {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start < end) {
+            swap(arr, start, end); // swap the two elements
+            start++;               // move start forward
+            end--;                 // move end backward
+        }
+        // stop when start >= end (pointers crossed)
     }
 }
