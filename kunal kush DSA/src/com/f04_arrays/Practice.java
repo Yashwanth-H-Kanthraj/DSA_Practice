bc87947d1e9092a7b06384719b246ecd1e32445b
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-06-04 21:43:02 +0530

    practice

--- kunal kush DSA/src/com/f04_arrays/Practice.java
+++ kunal kush DSA/src/com/f04_arrays/Practice.java
@@ -3,12 +3,12 @@ package com.f04_arrays;
 public class Practice {
     int[] arr = {18, 12, 9, 14, 77, 55};
 
-
     public static void main(String[] args) {
+
         Practice s = new Practice();
 
-       // s.search();
-        s.contains(14);
+        // s.search();
+        System.out.println("num is  at the index  " + (s.contains(14, s.arr)));
     }
 
     /*
@@ -21,23 +21,21 @@ public class Practice {
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) max = arr[i];
         }
-
         System.out.println(max + " @ the index ");
     }
 
     /* search in the array
      return index of array if it is present
      if not return -1 */
-    public void contains(int num) {
+    public int contains(int num, int[] arr) {
 
-        int doesItContains = 0;
+        for (int index = 0; index < arr.length; index++) {
+            int element = arr[index];
 
-        for (int i = 0; i < arr.length; i++) {
-
-            if (num == arr[i]) doesItContains = arr[i];
+            if (element == num) return index;
 
         }
-        System.out.println(doesItContains);
+        return -1;
     }
 }
 
