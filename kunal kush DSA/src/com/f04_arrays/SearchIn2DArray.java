d6eb20f30983de18feda32b2298dc6c9d7feb6c9
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-06-05 21:45:26 +0530

    practice

--- kunal kush DSA/src/com/f04_arrays/SearchIn2DArray.java
+++ kunal kush DSA/src/com/f04_arrays/SearchIn2DArray.java
@@ -1,20 +1,27 @@
 package com.f04_arrays;
 
+import java.util.Arrays;
+
 public class SearchIn2DArray {
+    int[][] arr = {
+            {23, 4, 64},
+            {36, 45, 89, 52, 71},
+            {78, 64, 845}
+    };
+
     public static void main(String[] args) {
-        int[][] arr = {
-                {23, 4, 64},
-                {36, 45, 89, 52},
-                {78, 64, 84}
-        };
+        SearchIn2DArray s = new SearchIn2DArray();
 
-        int target = 46;
+        int target = 71;
 
-        System.out.println(search(arr, target));
+       // System.out.println(Arrays.toString(search2(s.arr, target)));
+
+        s.search3();
 
     }
 
-    public static int search(int[][] arr, int target) {
+    // search a element in the 2d array
+    public static int search1(int[][] arr, int target) {
         for (int rows = 0; rows < arr.length; rows++) {
             for (int col = 0; col < arr[rows].length; col++) {
                 if (arr[rows][col] == target) return target;
@@ -22,6 +29,30 @@ public class SearchIn2DArray {
             }
         }
         return Integer.MAX_VALUE;
+    }
 
+    // search the element and return the row and column index
+    public static int[] search2(int[][] arr, int target) {
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[row][col] == target) {
+                    return new int[]{row, col};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public void search3() {
+        int maximum = arr[0][0];
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 1; col < arr[row].length; col++) {
+                if (arr[row][col] > maximum) {
+                    maximum = arr[row][col];
+                }
+            }
+        }
+        System.out.println(maximum);
     }
 }
