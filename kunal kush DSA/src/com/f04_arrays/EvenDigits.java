4d6c0b944d4eceefacd5f3eb5d51530cbe10b51c
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-06-08 21:35:16 +0530

    practice

--- kunal kush DSA/src/com/f04_arrays/EvenDigits.java
+++ kunal kush DSA/src/com/f04_arrays/EvenDigits.java
@@ -18,5 +18,18 @@ public class EvenDigits {
         return count;
     }
 
+    public static boolean Even(int num) {
+        int NumberOfDigits = DigitsInNumber(num);
+        return NumberOfDigits % 2 == 0;
+    }
+
+    static int DigitsInNumber(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+        return count;
+    }
 
 }
