e60e6eb2661c2b99f00c97c97bc79ef1fa0d2178
Yashwanth-H-Kanthraj <yashwanthkanthraj2@gmail.com>
2026-06-08 21:37:51 +0530

    practice

--- kunal kush DSA/src/com/f04_arrays/EvenDigits.java
+++ kunal kush DSA/src/com/f04_arrays/EvenDigits.java
@@ -1,35 +1,7 @@
 package com.f04_arrays;
 
 public class EvenDigits {
-    public static void main(String[] args) {
-
-        int[] nums = {12, 24, 54, 4564, 6, 847, 46};
-        System.out.println(logic(nums));
-
-    }
-
-    public static int logic(int[] nums) {
-        int count = 0;
-        for (int num : nums) {
-            if (Even(num)) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    public static boolean Even(int num) {
-        int NumberOfDigits = DigitsInNumber(num);
-        return NumberOfDigits % 2 == 0;
-    }
-
-    static int DigitsInNumber(int num) {
-        int count = 0;
-        while (num > 0) {
-            count++;
-            num = num / 10;
-        }
-        return count;
+    p
     }
 
 }
